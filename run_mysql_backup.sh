37b4b9fae2078f253465532a2bec1c51959e9e2d
root <root@localhost.localdomain>
2016-12-07 12:12:45 +0800

    add config tables auto backup

--- run_mysql_backup.sh
+++ run_mysql_backup.sh
@@ -1,9 +1,9 @@
-nohup python /root/htrd/zabbix_database_backup/historybackup.py 192.168.103.112 zabbix zabbix 3306 zabbix 127.0.0.1 htrd htrd 3306 htrd_bak history 2 /root/htrd/this_file_exist_stop_backup_history > /dev/null 2>log &
+nohup python /root/htrd/zabbix_database_backup/historybackup.py 192.168.103.112 zabbix zabbix 3306 zabbix 127.0.0.1 htrd htrd 3306 zabbix history 2 /root/htrd/this_file_exist_stop_backup_history > /dev/null 2>log &
 
-nohup python /root/htrd/zabbix_database_backup/historybackup.py 192.168.103.112 zabbix zabbix 3306 zabbix 127.0.0.1 htrd htrd 3306 htrd_bak history_uint 2 /root/htrd/this_file_exist_stop_backup_history_uint > /dev/null 2>log &
+nohup python /root/htrd/zabbix_database_backup/historybackup.py 192.168.103.112 zabbix zabbix 3306 zabbix 127.0.0.1 htrd htrd 3306 zabbix history_uint 2 /root/htrd/this_file_exist_stop_backup_history_uint > /dev/null 2>log &
 
-nohup python /root/htrd/zabbix_database_backup/historybackup.py 192.168.103.112 zabbix zabbix 3306 zabbix 127.0.0.1 htrd htrd 3306 htrd_bak history_text 2 /root/htrd/this_file_exist_stop_backup_history_text > /dev/null 2>log &
+nohup python /root/htrd/zabbix_database_backup/historybackup.py 192.168.103.112 zabbix zabbix 3306 zabbix 127.0.0.1 htrd htrd 3306 zabbix history_text 2 /root/htrd/this_file_exist_stop_backup_history_text > /dev/null 2>log &
 
-nohup python /root/htrd/zabbix_database_backup/historybackup.py 192.168.103.112 zabbix zabbix 3306 zabbix 127.0.0.1 htrd htrd 3306 htrd_bak history_str 2 /root/htrd/this_file_exist_stop_backup_history_str > /dev/null 2>log &
+nohup python /root/htrd/zabbix_database_backup/historybackup.py 192.168.103.112 zabbix zabbix 3306 zabbix 127.0.0.1 htrd htrd 3306 zabbix history_str 2 /root/htrd/this_file_exist_stop_backup_history_str > /dev/null 2>log &
 
-nohup python /root/htrd/zabbix_database_backup/historybackup.py 192.168.103.112 zabbix zabbix 3306 zabbix 127.0.0.1 htrd htrd 3306 htrd_bak history_log 2 /root/htrd/this_file_exist_stop_backup_history_log > /dev/null 2>log &
+nohup python /root/htrd/zabbix_database_backup/historybackup.py 192.168.103.112 zabbix zabbix 3306 zabbix 127.0.0.1 htrd htrd 3306 zabbix history_log 2 /root/htrd/this_file_exist_stop_backup_history_log > /dev/null 2>log &
